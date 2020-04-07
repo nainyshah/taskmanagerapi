e5836a4a2b9dc48b2ca0616ce15b16b3b5969702
Syed Raza <shussnain.raza@gmail.com>
2020-04-07 14:42:30 +0300

    removing unnecessary code lines from index.js

--- src/index.js
+++ src/index.js
@@ -9,15 +9,6 @@ const taskRouters = require("./routers/task");
 const app = express();
 const port = process.env.PORT;
 
-const multer = require("multer");
-const upload = multer({
-  dest: "images"
-});
-
-app.post("/upload", upload.single("upload"), (req, res) => {
-  res.send();
-});
-
 // app.use((req, res, next) => {
 //   console.log(req.method, req.path);
 //   next();
@@ -38,11 +29,11 @@ app.listen(port, () => {
   console.log("Server is up on port", port);
 });
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const myFunction = async () => {
-  const token = jwt.sign({ _id: "abc123" }, "aquickbrownfox");
-  console.log(token);
-};
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "abc123" }, "aquickbrownfox");
+//   console.log(token);
+// };
 
-myFunction();
+// myFunction();
